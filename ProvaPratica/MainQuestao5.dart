3ff42c8bf5f14721edc25a218eb5aefae66c4df3
Audrey Cristhiny <audreykochinski@gmail.com>
2026-04-03 21:49:44 -0300

    Create MainQuestao5.dart

---
+++ ProvaPratica/MainQuestao5.dart
@@ -0,0 +1,47 @@
+import 'Pokemon.dart';
+import 'Pokedex.dart';
+
+void main() {
+  Pokedex pokedex = Pokedex();
+
+  Pokemon pokemon1 = Pokemon(001, 'Bulbasaur', 'Planta e Veneno', 15, 250, 294, true);
+  pokemon1.proximaEvolucao = 'Ivysaur';
+  pokemon1.nivelEvolucao = 16; 
+
+  Pokemon pokemon2 = Pokemon(007, 'Squirtle', 'Água', 12, 270, 292, false);
+
+  Pokemon pokemon3 = Pokemon(039, 'Jigglypuff', 'Normal e Fada', 18, 400, 434, true);
+  pokemon3.proximaEvolucao = 'Wigglytuff';
+  pokemon3.nivelEvolucao = 18; 
+
+  Pokemon pokemon4 = Pokemon(025, 'Pikachu', 'Elétrico', 25, 230, 274, true);
+  pokemon4.proximaEvolucao = 'Raichu';
+  pokemon4.nivelEvolucao = 20; 
+
+  Pokemon pokemon5 = Pokemon(052, 'Meowth', 'Normal', 15, 260, 284, false);
+
+  pokedex.adicionarPokemon(pokemon1);
+  pokedex.adicionarPokemon(pokemon2);
+  pokedex.adicionarPokemon(pokemon3);
+  pokedex.adicionarPokemon(pokemon4);
+  pokedex.adicionarPokemon(pokemon5);
+
+  // - Função local para exibir o resultado de cada filtro
+  void mostrarFiltro(String titulo, List<Pokemon> lista) {
+    print('\n------------------------------------------------------------------');
+    print('$titulo:');
+    if (lista.isEmpty) {
+      print('Nenhum resultado.');
+      return;
+    }
+    for (int i = 0; i < lista.length; i++) {
+      print('  - #${lista[i].numero} ${lista[i].nome} (${lista[i].tipo}) Nv.${lista[i].nivel}');
+    }
+  }
+
+  mostrarFiltro('Capturados', pokedex.listarCapturados());
+  mostrarFiltro('Tipo: Normal e Fada', pokedex.listarPorTipo('normal e fada')); 
+  mostrarFiltro('Tipo: NORMAL E FADA', pokedex.listarPorTipo('NORMAL E FADA')); 
+  mostrarFiltro('Pokemons acima do nível 15', pokedex.listarAcimaDoNivel(15));
+  mostrarFiltro('Pokemons que podem evoluir agora', pokedex.listarQuePodemEvoluir());
+}
