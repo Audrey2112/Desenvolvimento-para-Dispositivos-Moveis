ff432ebe15082407381299f1990a556f096ec27c
Audrey Cristhiny <audreykochinski@gmail.com>
2026-05-27 16:27:00 -0300

    Update pokemons_page.dart

--- ProvaPratica3/lib/features/pokemons/pokemons_page.dart
+++ ProvaPratica3/lib/features/pokemons/pokemons_page.dart
@@ -2,29 +2,29 @@
 
 import 'package:flutter/material.dart';
 
-import '../../app/app_routes.dart';
-import '../../shared/models/pokemon.dart';
-import 'widget/pokemon_card.dart';
+import '../../app/app_routes.dart'; // - Importa o arquivo de rotas para navegação - (Questão 02)
+import '../../shared/models/pokemon.dart'; // - Importa a model Pokemon criada no projeto - (Questão 04)
+import 'widget/pokemon_card.dart'; // - Importa o widget customizado do card de Pokémon - (Questão 06)
 
 class PokemonsPage extends StatelessWidget {
   const PokemonsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated( 
+    return ListView.separated( // - Lista rolável, o separated adiciona espaçamento automático entre os itens
       padding: const EdgeInsets.all(16),
-      itemCount: pokemonsFakes.length, // - pega os pokemons fakes criados na shared -> models -> arquivo pokemon
-      separatorBuilder: (_, __) => const SizedBox(height: 12),
-      itemBuilder: (context, index) {
+      itemCount: pokemonsFakes.length, // - pega os pokemons fakes criados na shared -> models -> arquivo pokemon - (Questão 07)
+      separatorBuilder: (_, __) => const SizedBox(height: 12), // - Espaçamento entre os cards
+      itemBuilder: (context, index) { // - Responsável por construir cada item da lista
         final pokemon = pokemonsFakes[index];
 
-        return PokemonCard(
+        return PokemonCard( // - Retorna o card personalizado do Pokémon - (Questão 06)
           pokemon: pokemon,
-          onTap: () {
-            Navigator.pushNamed(
+          onTap: () { // - Evento de clique do card onde se navega para a tela de detalhes - (Questão 09)
+            Navigator.pushNamed( // - Navegação utilizando rotas nomeadas, envia o pokemon selecionado - (Questão 09)
               context,
-              AppRoutes.detalhesPokemon,
-              arguments: pokemon,
+              AppRoutes.detalhesPokemon, // - Nome da rota
+              arguments: pokemon, // - Envia o Pokémon
             );
           },
         );
